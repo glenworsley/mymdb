b74d9f3a79a9ccb66f88e529da9bcf12679c93e4
Glen Worsley <glen.worsley@gmail.com>
2020-05-05 15:49:40 +1000

    add inital version of api

---
+++ src/graphql/queries.js
@@ -0,0 +1,28 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getMovie = /* GraphQL */ `
+  query GetMovie($id: ID!) {
+    getMovie(id: $id) {
+      id
+      title
+      genre
+    }
+  }
+`;
+export const listMovies = /* GraphQL */ `
+  query ListMovies(
+    $filter: ModelMovieFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listMovies(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        title
+        genre
+      }
+      nextToken
+    }
+  }
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateMovie = /* GraphQL */ `
+  subscription OnCreateMovie {
+    onCreateMovie {
+      id
+      title
+      genre
+    }
+  }
+`;
+export const onUpdateMovie = /* GraphQL */ `
+  subscription OnUpdateMovie {
+    onUpdateMovie {
+      id
+      title
+      genre
+    }
+  }
+`;
+export const onDeleteMovie = /* GraphQL */ `
+  subscription OnDeleteMovie {
+    onDeleteMovie {
+      id
+      title
+      genre
+    }
+  }
+`;
